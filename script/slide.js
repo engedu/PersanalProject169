6c985347bec582f11051f57c6138ccfadd76815a
Danai thab-am <30897050+duck15423@users.noreply.github.com>
2018-05-19 19:51:14 +0700

    Personal project new

--- script/slide.js
+++ script/slide.js
@@ -1,14 +1,14 @@
-var myIndex = 0;
-carousel();
+var myIndex = 0;/*ประกาศตัวแปร*/
+carousel();/*ปลักอินการแสดงผล*/
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slide");
-    for (i = 0; i < x.length; i++) {
+    var i;/*ประกาศตัวแปร*/
+    var x = document.getElementsByClassName("slide");/*ให้ดึงข้อมูลชื่อclass slide*/
+    for (i = 0; i < x.length; i++) {/*.ให้แสดงข้อมูลแบบลูป*/
        x[i].style.display = "none";  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2500);    
+    setTimeout(carousel, 8000);    /*ระยะเวลาการแสดงผลต่อ 1ภาพ //8วินาที*/
 }
